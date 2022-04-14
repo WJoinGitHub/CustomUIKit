12a6c66ad8646763dc899d63c6e07aca15bcd3f7
wxh <704276893@qq.com>
2022-04-14 17:29:55 +0800

    示例代码

--- CustomUIKit/Classes/ViewController/Target_A.h
+++ CustomUIKit/Classes/ViewController/Target_A.h
@@ -11,6 +11,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Target_A : NSObject
 
+/// 获取该组件的AViewController 的实例化对象
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- CustomUIKit/Classes/ViewController/Target_A.m
+++ CustomUIKit/Classes/ViewController/Target_A.m
@@ -6,7 +6,13 @@
 //
 
 #import "Target_A.h"
+#import "TestVC.h"
 
 @implementation Target_A
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params {
+    TestVC *viewController = [[TestVC alloc] init];
+    return viewController;
+}
 
 @end
