11c1a6cbbc11b4ffd91ed1aedebf3ce2d5de9cf1
wxh <704276893@qq.com>
2022-04-14 17:29:28 +0800

    测试文件

---
+++ CustomUIKit/Classes/ViewController/Target_A.h
@@ -0,0 +1,16 @@
+//
+//  Target_A.h
+//  CustomUIKit
+//
+//  Created by tofuls on 2022/4/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Target_A : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomUIKit/Classes/ViewController/Target_A.m
@@ -0,0 +1,12 @@
+//
+//  Target_A.m
+//  CustomUIKit
+//
+//  Created by tofuls on 2022/4/14.
+//
+
+#import "Target_A.h"
+
+@implementation Target_A
+
+@end
