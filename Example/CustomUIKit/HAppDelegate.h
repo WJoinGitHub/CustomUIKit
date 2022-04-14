9b668642ff90980e1765d79bddad45ee99ee34fe
wxh <704276893@qq.com>
2022-04-14 10:48:08 +0800

    Initial commit

---
+++ Example/CustomUIKit/HAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HAppDelegate.h
+//  CustomUIKit
+//
+//  Created by wxh on 04/14/2022.
+//  Copyright (c) 2022 wxh. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
